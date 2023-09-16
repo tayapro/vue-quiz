d866e7046ee43960292e3dddea418563be6f3ca6
Taya Simakova <stipaxa@gmail.com>
2023-09-16 15:51:45 +0100

    made nicer about page

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,62 +2,56 @@
     <div class="container">
         <div class="center">
             <div>
-                <h1>ABOUT</h1>
-                <img src="@/logo/quiz.jpg" alt="user" width="150" />
+                <div class="header">
+                    <h1>ABOUT</h1>
+                </div>
+                <div class="img">
+                    <img src="@/logo/quiz.png" alt="user" width="150" />
+                </div>
             </div>
-            <div class="span-center">
+            <div class="span">
                 <span
                     >Lorem ipsum dolor sit amet consectetur, adipisicing elit.
                     Natus, nobis accusantium, culpa voluptatem error possimus
                     quo beatae ducimus hic officia magni deserunt repellat
-                    dolores! Dolores quis a consequatur non beatae. Lorem ipsum
-                    dolor sit amet consectetur adipisicing elit. Facere nobis,
-                    aut laborum sint animi, ipsum officia quod aspernatur est
-                    voluptatum iusto consequatur maiores optio doloribus saepe.
-                    Et iure nemo eaque.</span
-                >
+                    dolores!
+                </span>
             </div>
         </div>
     </div>
 </template>
 
 <style scoped>
-.score {
-    font-size: 1.3rem;
-    display: inline-block;
-    align-content: center;
+.img {
+    margin-right: 1rem;
+    max-width: inherit;
+    max-height: inherit;
+    height: inherit;
+    width: inherit;
+    object-fit: cover;
 }
 
 .center {
     display: flex;
     justify-content: center;
-    align-items: center;
+    align-items: flex-end;
 }
-.span-center {
+.span {
     display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: aqua;
-    margin: 1rem;
+    margin-left: 1rem;
+    margin-bottom: 3rem;
     width: 60%;
     padding: 1rem;
-    margin: 1rem;
-    margin-bottom: 0;
     border-radius: 15px;
-    background-color: rgba(202, 202, 202, 0.377);
+    background-color: rgba(238, 236, 236, 0.8);
     box-shadow: rgba(202, 202, 202, 0.35) 0px 5px 15px;
-}
-
-.card-center {
-    display: flex;
-    justify-content: center;
-    align-items: center;
 }
 
 .container {
     display: flex;
     flex-direction: column;
-    justify-content: center;
+    /* justify-content: center; */
+    padding-top: 7rem;
     height: 100dvh;
 }
 </style>
